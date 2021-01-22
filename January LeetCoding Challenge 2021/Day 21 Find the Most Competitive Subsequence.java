c2b89ce4497f9e6784821067c64cb3ba6d3c77d8
GayChin <56284497+GayChin@users.noreply.github.com>
2021-01-22 13:20:16 +0800

    Update Day 21 Find the Most Competitive Subsequence.java

--- January LeetCoding Challenge 2021/Day 21 Find the Most Competitive Subsequence.java
+++ January LeetCoding Challenge 2021/Day 21 Find the Most Competitive Subsequence.java
@@ -1,9 +1,14 @@
+// After we pop the last element from stack,
+// we have stack.size() - 1 in the stack,
+// there are A.size() - i can still be pushed.
+// if stack.size() - 1 + A.size() - i >= k, we can pop the stack.
+    
 class Solution {
     public int[] mostCompetitive(int[] nums, int k) {
     Stack<Integer> stack = new Stack<>();
     int[] result = new int[k];
     for (int i = 0; i < nums.length; i++) {
-        while (!stack.empty() && nums[i] < nums[stack.peek()] && nums.length - i + stack.size() > k) {
+        while (!stack.empty() && nums[i] < nums[stack.peek()] && stack.size()-1 + nums.length-i >= k) { 
             stack.pop();
         }
         if (stack.size() < k) {
