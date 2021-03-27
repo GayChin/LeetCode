713dd1803fd50c187701f0d3d0bbc8221a132688
GayChin <56284497+GayChin@users.noreply.github.com>
2021-03-27 17:34:44 +0800

    Update Day 19 Longest Palindromic Substring.java

--- January LeetCoding Challenge 2021/Day 19 Longest Palindromic Substring.java
+++ January LeetCoding Challenge 2021/Day 19 Longest Palindromic Substring.java
@@ -1,25 +1,17 @@
-class Solution {
-private int lo, maxLen;
-
-public String longestPalindrome(String s) {
-	int len = s.length();
-	if (len < 2)
-		return s;
-	
-    for (int i = 0; i < len-1; i++) {
-     	extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible
-     	extendPalindrome(s, i, i+1); //assume even length.
+    public String longestPalindrome(String s) {
+        String max = "";
+        for (int i = 0; i < s.length(); i++) {
+            String s1 = extend(s, i, i), s2 = extend(s, i, i + 1);
+            if (s1.length() > max.length()) max = s1; 
+            if (s2.length() > max.length()) max = s2;
+        }
+        return max;
     }
-    return s.substring(lo, lo + maxLen);
-}
-
-private void extendPalindrome(String s, int j, int k) {
-	while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
-		j--;
-		k++;
-	}
-	if (maxLen < k - j - 1) {
-		lo = j + 1;
-		maxLen = k - j - 1;
-	}
-}}
+    
+//extend function starts at a particular point and extend point i backward and j point forward to check the substring is palindromic or not
+    private String extend(String s, int i, int j) {
+        for (; 0 <= i && j < s.length(); i--, j++) {
+            if (s.charAt(i) != s.charAt(j)) break;
+        }
+        return s.substring(i + 1, j);
+    }
