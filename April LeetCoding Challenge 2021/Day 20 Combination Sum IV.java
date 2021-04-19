734675e9dd49b7912178bb2270d98f4a0124cfa6
GayChin <56284497+GayChin@users.noreply.github.com>
2021-04-20 01:23:52 +0800

    Create Day 20 Combination Sum IV.java

---
+++ April LeetCoding Challenge 2021/Day 20 Combination Sum IV.java
@@ -0,0 +1,18 @@
+class Solution {
+     public int combinationSum4(int[] nums, int target) {
+            if(nums == null || nums.length ==0){
+                return 0;
+            }
+            //dp[i] will represent the number of ways we can reach i from the starting point (dp[0]). This means that dp[T] will represent our final solution.
+            int[] dp = new int[target+1];
+            dp[0]=1; 
+            for(int sum=0; sum<=target; sum++) {
+                for(int num : nums) {
+                    dp[sum] += sum-num>=0 ? dp[sum-num] : 0;
+
+                } 
+            }
+            return dp[target];
+        }
+
+}
