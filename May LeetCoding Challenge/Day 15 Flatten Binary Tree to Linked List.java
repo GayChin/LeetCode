3cfe16870cb085820b0e991848afe749baf476b7
GayChin <56284497+GayChin@users.noreply.github.com>
2021-05-15 01:49:04 +0800

    Update Day 15 Flatten Binary Tree to Linked List.java

--- May LeetCoding Challenge/Day 15 Flatten Binary Tree to Linked List.java
+++ May LeetCoding Challenge/Day 15 Flatten Binary Tree to Linked List.java
@@ -13,3 +13,17 @@ class Solution {
     }
 }
 // Basically set current node as the right node of the next root node.
+
+public void flatten(TreeNode root) {
+	TreeNode cur = root;
+	while (cur != null) {
+		if (cur.left != null) {
+			TreeNode last = cur.left;
+			while (last.right != null) last = last.right;
+			last.right = cur.right;
+			cur.right = cur.left;
+			cur.left = null;
+		}
+		cur = cur.right;
+	}
+}
